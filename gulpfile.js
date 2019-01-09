74541d8a9ac1dc1755c31917c524548dd3b4d08a
yinm <yinm.cminor9@gmail.com>
2019-01-09 22:00:29 +0900

    Add bundle settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var nodemon = require('gulp-nodemon');
+var browserify = require('browserify')
+var source = require('vinyl-source-stream')
 var sequence = require('run-sequence');
 
 gulp.task('copy', function () {
@@ -17,9 +19,21 @@ gulp.task('compile', function () {
 });
 
 gulp.task('watch', function () {
-  gulp.watch('src/**/*.js', ['compile']);
+  gulp.watch('src/**/*.js', ['compile', 'bundle']);
   gulp.watch('src/**/*.html', ['copy']);
 });
+
+gulp.task('bundle', function() {
+  var b = browserify({
+    entries: 'src/index.js',
+    debug: true
+  })
+    .transform('babelify', { presets: ['es2015'] })
+
+  return b.bundle()
+    .pipe(source('build/application.js'))
+    .pipe(gulp.dest('dist'))
+})
 
 gulp.task('start', function () {
   nodemon({
@@ -31,5 +45,5 @@ gulp.task('start', function () {
 });
 
 gulp.task('default', function (callback) {
-  sequence(['compile', 'watch', 'copy'], 'start', callback);
+  sequence(['compile', 'watch', 'copy', 'bundle'], 'start', callback);
 });
