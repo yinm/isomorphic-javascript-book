8cc05099c174a0eed5960b3161ff00b3cc8e21c6
yinm <yinm.cminor9@gmail.com>
2019-01-10 21:49:10 +0900

    Add Controller

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import Hapi from 'hapi';
-import nunjucks from 'nunjucks'
 import Application from './lib'
-
-nunjucks.configure('./dist')
+import Controller from './lib/controller'
 
 const server = new Hapi.Server();
 server.connection({
@@ -10,27 +8,8 @@ server.connection({
   port: 8000
 });
 
-function getName(request) {
-  let name = {
-    fname: 'Rick',
-    lname: 'Sanchez'
-  }
-
-  let nameParts = request.params.name
-    ? request.params.name.split('/')
-    : []
-
-  name.fname = (nameParts[0] || request.query.fname) || name.fname
-  name.lname = (nameParts[1] || request.query.lname) || name.lname
-  return name
-}
-
 const application = new Application({
-  '/': function (request, reply) {
-    nunjucks.render('index.html', getName(request), function (err, html) {
-      reply(html)
-    })
-  }
+  '/': Controller
 }, {
   server: server
 })
---
+++ src/lib/controller.js
@@ -0,0 +1,13 @@
+export default class Controller {
+  constructor(context) {
+    this.context = context
+  }
+
+  index(application, request, reply, callback) {
+    callback(null)
+  }
+
+  toString(callback) {
+    callback(null, 'success')
+  }
+}
