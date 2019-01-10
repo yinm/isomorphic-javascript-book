b3e8d3e7e3e95fd99084399a77cfa685f97b3aaa
yinm <yinm.cminor9@gmail.com>
2019-01-10 22:22:57 +0900

    Add bundle

--- src/index.js
+++ src/index.js
@@ -11,17 +11,29 @@ server.connection({
   port: 8000
 });
 
+const APP_FILE_PATH = '/application.js'
 const application = new Application({
   '/hello/{name*}': HelloController
 }, {
   server: server,
   document: function (application, controller, request, reply, body, callback) {
-    nunjucks.render('./index.html', { body: body }, (err, html) => {
+    nunjucks.render('./index.html', {
+      body: body,
+      application: APP_FILE_PATH
+    }, (err, html) => {
       if (err) {
         return callback(err, null)
       }
       callback(null, html)
     })
+  }
+})
+
+server.route({
+  method: 'GET',
+  path: APP_FILE_PATH,
+  handler: (request, reply) => {
+    reply.file('dist/build/application.js')
   }
 })
 
