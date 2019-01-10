89807b7911b5a30637f5c1ed014580986ff3d4e6
yinm <yinm.cminor9@gmail.com>
2019-01-11 08:46:42 +0900

    Add routing

--- src/lib/index.client.js
+++ src/lib/index.client.js
@@ -1,8 +1,37 @@
+import Call from 'call'
+
 export default class Application {
+  constructor(routes, options) {
+    this.routes = routes
+    this.options = options
+    this.router = new Call.Router()
+    this.registerRoutes(routes)
+  }
+
+  registerRoutes(routes) {
+    for (let path in routes) {
+      this.router.add({
+        path: path,
+        method: 'get'
+      })
+    }
+  }
+
   navigate(url, push=true) {
     if (!history.pushState) {
       window.location = url
       return
+    }
+
+    let urlParts = url.split('?')
+    let [path, search] = urlParts
+    let match = this.router.route('get', path)
+    let { route, params } = match
+    let Controller = this.routes[route]
+
+    if (route && Controller) {
+      console.log(match)
+      console.log(Controller)
     }
 
     console.log(url)
