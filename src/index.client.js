cde725ca7231dc59571c57a0c2123d3c5ab70d64
yinm <yinm.cminor9@gmail.com>
2019-01-11 09:18:30 +0900

    Add response

--- src/index.client.js
+++ src/index.client.js
@@ -1,5 +1,8 @@
 import Application from './lib'
 import HelloController from './hello-controller'
+import nunjucks from 'nunjucks'
+
+nunjucks.configure('/template')
 
 const application = new Application({
   '/hello/{name*}': HelloController
