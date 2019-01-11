5d396d2c62f3d733d59f944b8ccb1b03709a8826
yinm <yinm.cminor9@gmail.com>
2019-01-11 09:36:25 +0900

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,51 +1,12 @@
-import Hapi from 'hapi';
 import Application from './lib'
 import HelloController from './hello-controller'
 import nunjucks from 'nunjucks'
-import path from 'path'
+import options from './options'
 
-nunjucks.configure('./dist', {autoescape: false})
+nunjucks.configure(options.nunjucks, {autoescape: false})
 
-const server = new Hapi.Server();
-server.connection({
-  host: 'localhost',
-  port: 8000
-});
-
-const APP_FILE_PATH = '/application.js'
 const application = new Application({
   '/hello/{name*}': HelloController
-}, {
-  server: server,
-  document: function (application, controller, request, reply, body, callback) {
-    nunjucks.render('./index.html', {
-      body: body,
-      application: APP_FILE_PATH
-    }, (err, html) => {
-      if (err) {
-        return callback(err, null)
-      }
-      callback(null, html)
-    })
-  }
-})
-
-server.route({
-  method: 'GET',
-  path: APP_FILE_PATH,
-  handler: (request, reply) => {
-    reply.file('dist/build/application.js')
-  }
-})
-
-server.route({
-  method: 'GET',
-  path: '/template/{template*}',
-  handler: {
-    file: (request) => {
-      return path.join('dist', request.params.template)
-    }
-  }
-})
+}, options)
 
 application.start()
