9d4599d28d1b42d198722643c87ff71d6da746fb
yinm <yinm.cminor9@gmail.com>
2019-01-12 21:37:27 +0900

    Check rehydrate

--- src/hello-controller.js
+++ src/hello-controller.js
@@ -1,6 +1,10 @@
 import Controller from './lib/controller'
 import nunjucks from 'nunjucks'
 
+function onClick(e) {
+  console.log(e.currentTarget)
+}
+
 function getName(context) {
   let name = {
     fname: 'Rick',
@@ -19,17 +23,29 @@ function getName(context) {
 
 export default class HelloController extends Controller {
   index(application, request, reply, callback) {
-    this.context.cookie.set('random', '_' + (Math.floor(Math.random() * 1000) + 1), {path: '/'})
+    this.context.data = { random: Math.floor(Math.random() * 1000) + 1 }
     callback(null)
   }
 
   toString(callback) {
-    nunjucks.render('hello.html', getName(this.context), (err, html) => {
+    let context = getName(this.context)
+    context.data = this.context.data
+
+    nunjucks.render('hello.html', context, (err, html) => {
       if (err) {
         return callback(err, null)
       }
 
       callback(null, html)
     })
+  }
+
+  attach(el) {
+    console.log(this.context.data.random)
+    this.clickHandler = el.addEventListener('click', onClick, false)
+  }
+
+  detach(el) {
+    el.removeEventListener('click', onClick, false)
   }
 }
