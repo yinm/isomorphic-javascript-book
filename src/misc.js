72eaf50ad9ee3131a0d51ae87edceb4191cb66db
yinm <yinm.cminor9@gmail.com>
2019-01-12 18:14:43 +0900

    Check Hapi

--- src/misc.js
+++ src/misc.js
@@ -1,9 +1,31 @@
-import http from 'http'
+import Hapi from 'hapi'
 
-http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Set-Cookie': 'some-cookie=some value',
-    'Content-Type': 'text/plain'
-  })
-  res.end('Hello\n')
-}).listen(8080)
+const server = new Hapi.Server({
+  debug: {
+    request: ['error']
+  }
+})
+server.connection({
+  host: 'localhost',
+  port: 8080
+})
+
+server.state('some-cookie', {
+  ttl: null,
+  isSecure: true,
+  isHttpOnly: true,
+  encoding: 'base64json',
+  clearInvalid: false, // remove invalid cookies
+  strictHeader: true // don't allow violations of RFC 6265
+})
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler(req, res) {
+    res.state('some-cookie', {someValue: false})
+    res('Hello\n')
+  }
+})
+
+server.start()
